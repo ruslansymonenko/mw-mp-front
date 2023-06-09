906f24152d559b6ffc38a2edf8bcb4c530f0e031
Ruslan Symonenko <symonenkors@gmail.com>
2023-06-09 17:13:38 +0300

    creating the basic structure of the frontend application

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import { Helmet } from 'react-helmet';
+
+const HomePage: React.FC = () => {
+  return <>
+    <Helmet>
+      <title>Головна  - MarketPlace</title>
+    </Helmet>
+
+    <h1>Головна - MarketPlace</h1>
+  </>
+}
+
+export default HomePage
